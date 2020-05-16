1c6cb52c12810b153cbb0c52d5445ead32e27bac
Victor Stati <victorstti@outlook.com>
2020-05-16 15:33:26 -0300

    Atualizacao: utilizando somente JSonIgnore

--- src/main/java/com/stati/cursomc/domain/Endereco.java
+++ src/main/java/com/stati/cursomc/domain/Endereco.java
@@ -8,9 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco implements Serializable{
@@ -26,7 +25,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
