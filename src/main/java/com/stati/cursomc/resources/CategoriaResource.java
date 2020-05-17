0a068f9900ccf9478b97b002ec66515994e2765c
Victor Stati <victorstti@outlook.com>
2020-05-17 09:18:34 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/stati/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/stati/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,8 @@
 package com.stati.cursomc.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.stati.cursomc.domain.Categoria;
+import com.stati.cursomc.dto.CategoriaDTO;
 import com.stati.cursomc.services.CategoriaService;
 
 @RestController
@@ -27,6 +28,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
--- src/main/java/com/stati/cursomc/services/CategoriaService.java
+++ src/main/java/com/stati/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.stati.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(()->new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 	public Categoria insert(Categoria categoria) {
