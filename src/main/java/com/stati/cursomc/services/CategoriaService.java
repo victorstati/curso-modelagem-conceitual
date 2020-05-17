4ca2384c6c3206edc3badafde903dcf62b6af6d7
Victor Stati <victorstti@outlook.com>
2020-05-17 09:49:02 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/stati/cursomc/services/CategoriaService.java
+++ src/main/java/com/stati/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.stati.cursomc.domain.Categoria;
+import com.stati.cursomc.dto.CategoriaDTO;
 import com.stati.cursomc.repositories.CategoriaRepository;
 import com.stati.cursomc.services.exceptions.DataIntegrityException;
 import com.stati.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,6 +53,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDto) {
+		return new Categoria(categoriaDto.getId(), categoriaDto.getNome());
 	}
 
 }
