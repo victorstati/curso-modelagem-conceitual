ebc4c35d5c4f3f22a76ad3a11e8cbec24492bacd
Victor Stati <victorstti@outlook.com>
2020-05-19 20:21:26 -0300

    Ajustando e testando delecao de cliente

--- src/main/java/com/stati/cursomc/services/ClienteService.java
+++ src/main/java/com/stati/cursomc/services/ClienteService.java
@@ -60,7 +60,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma cliente que possui pedidos");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 	
