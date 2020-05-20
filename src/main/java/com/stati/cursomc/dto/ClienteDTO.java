26ace6dfc7a1b433bfcdf56bf71f6a7e5af469c3
Victor Stati <victorstti@outlook.com>
2020-05-20 09:01:44 -0300

    Validacao customizada email nao repetido na atualizacao de cliente

--- src/main/java/com/stati/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/stati/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.stati.cursomc.domain.Cliente;
+import com.stati.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
